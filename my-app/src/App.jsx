5bc71096073515f0c58619158d6589129d7c59af
sebie12 <sebae122002@gmail.com>
2025-10-22 16:42:45 +0100

    programmed the fall of the block

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -5,28 +5,14 @@ import FloorMenu from './FloorMenu'
 
 
 function App() {
-  const [timer, setTimer] = useState(0);
-
-    useEffect(() => {
-        const interval = setInterval(() => {
-            setTimer(prev => prev + 1);
-        }, 1000);
-
-        return () => clearInterval(interval);
-    }, []);
-
-    const position = {
-        bottom: '{timer}px'
-    };
-
-    const clickFun = () => {
-        setTimer(0);
-    }
 
   return (
     <>
-     <Block style = {position} onClick = {clickFun}></Block>
-     <FloorMenu></FloorMenu>
+      <div className='main'>
+        <Block></Block>
+      </div>
+     
+     
     </>
   )
 }
--- my-app/src/Block.jsx
+++ my-app/src/Block.jsx
@@ -4,9 +4,33 @@ import './Block.css'
 
 
 function Block() {
+    const [timer, setTimer] = useState(0);
 
+    useEffect(() => {
+        const interval = setInterval(() => {
+            setTimer(prev => prev + 0.1);
+        }, 10);
+
+        return () => clearInterval(interval);
+    }, []);
+
+    const [maxHeight, setMaxHeight] = useState(() => (typeof window !== 'undefined' ? window.innerHeight : 1000));
+
+    useEffect(() => {
+        const onResize = () => setMaxHeight(window.innerHeight);
+        window.addEventListener('resize', onResize);
+        return () => window.removeEventListener('resize', onResize);
+    }, []);
+
+    const position = {
+        bottom: `${Math.max(0, maxHeight - 9.85 * timer * timer)}px`
+    };
+
+    const clickFun = () => {
+        setTimer(0);
+    }
     return(
-        <div className="block">Hello</div>
+        <div className="block" style={position} onClick = {clickFun} onPo>Hello</div>
     )
 }
 
